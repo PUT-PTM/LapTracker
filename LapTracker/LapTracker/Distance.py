3bb212506ac43d979335e13aab91921905d12a10
Marcin Wiktorowski <wiktorowski211@gmail.com>
2018-04-17 09:39:06 +0200

    Distance counting in simulator

--- LapTracker/LapTracker/Distance.py
+++ LapTracker/LapTracker/Distance.py
@@ -18,14 +18,15 @@ def haversine(lon1, lat1, lon2, lat2):
     c = 2 * asin(sqrt(a)) 
     # Radius of earth in kilometers is 6371
     km = 6371* c
-    return km/1000
+    return km
 
 """
 Less precision, much faster, requires testing if it's enough
 """
 def equirectangular_dist_approx(lon1, lat1, lon2, lat2):
-	R = 6371 
+	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+	R = 6371
 	x = (lon2 - lon1) * cos( 0.5*(lat2+lat1) )
 	y = lat2 - lat1
 	d = R * sqrt( x*x + y*y )
-	return d/1000
+	return d
--- LapTracker/LapTracker/Simulator.py
+++ LapTracker/LapTracker/Simulator.py
@@ -2,6 +2,14 @@ import csv
 import threading
 import time
 
+from Distance import *
+
+class Packet(object):
+    def __init__(self, lat, lon, date):
+        self.lat = float(lat)
+        self.lon = float(lon)
+        self.date = date
+
 class Simulator(object):
     def __init__(self):
         self.file = open('Tracks/gpsdata.csv','rt')
@@ -10,14 +18,22 @@ class Simulator(object):
     def __del__(self):
         self.file.close()
 
-    def get_row(self):
-        first = next(self.rows, None)
-        if first:
-            print (first)
-            time.sleep(1)
+    def get_current(self):
+        row = next(self.rows, None)
+        if row:
+            packet = Packet(row[0],row[1],row[2])
+            return packet
         else:
             self.file.seek(0)
+            return self.get_current()
 
 sim = Simulator()
 while True:
-    sim.get_row()
+    packet1 = sim.get_current()
+    packet2 = sim.get_current()
+
+    distance_haver = haversine(packet1.lon, packet1.lat, packet2.lon, packet2.lat)
+    distance_equire = equirectangular_dist_approx(packet1.lon, packet1.lat, packet2.lon, packet2.lat)
+
+    print("Distance haver: " + str(distance_haver) + " Distance equire: " + str(distance_equire))
+    time.sleep(1)
