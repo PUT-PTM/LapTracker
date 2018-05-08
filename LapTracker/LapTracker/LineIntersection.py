3daf430119a5edc3f71a4562c9d5cda0da6f7c46
Marcin Wiktorowski <wiktorowski211@gmail.com>
2018-05-08 16:36:40 +0200

    Intersection & simulator (legends proclaim Rzar8767 was here)

---
+++ LapTracker/LapTracker/LineIntersection.py
@@ -0,0 +1,44 @@
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1]) #Typo was here
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       return 'No intersection'
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
+
+def contains(segment, ps):
+    Min = (min(segment[0][0], segment[1][0]), min(segment[0][1], segment[1][1]))
+    Max = (max(segment[0][0], segment[1][0]), max(segment[0][1], segment[1][1]))
+    if (Min[0] <= ps[0] <= Max[0] and Min[1] <= ps[1] <= Max[1]):
+        return True
+    else:
+        return False
+
+def intersects(segment1, segment2):
+    R = line_intersection(segment1, segment2)
+    return contains(segment1, R) & contains(segment2,R)
+
+#A = (0,2)
+#B = (6,2)
+#C = (4,3)
+#D = (4,8)
+
+#print (intersects((A,B),(C,D)))
+#Motor2 = False
+#if Motor:
+#    Motor2 = contains(C, D, R)
+
+#if Motor2:
+#    print ("Intersection detected:", Motor2)
+#else:
+#    print ("No single intersection point detected")
+
+
--- LapTracker/LapTracker/Simulator.py
+++ LapTracker/LapTracker/Simulator.py
@@ -3,6 +3,7 @@ import threading
 import time
 
 from Distance import *
+from LineIntersection import *
 
 class Packet(object):
     def __init__(self, lat, lon, date):
@@ -24,18 +25,29 @@ class Simulator(object):
             packet = Packet(row[0],row[1],row[2])
             return packet
         else:
+            print("EOF ##############################################################")
             self.file.seek(0)
             return self.get_current()
 
 sim = Simulator()
-finish = Packet(52.323239833,17.6696305, '')
+finish = Packet(52.323486, 17.668115, '')
+A = (52.323632, 17.668220)
+B = (52.323384, 17.668048)
+packet1 = sim.get_current()
 while True:
-    packet1 = sim.get_current()
-    distance_haver = haversine(packet1.lon, packet1.lat, finish.lon, finish.lat)
-    distance_equire = equirectangular_dist_approx(packet1.lon, packet1.lat, finish.lon, finish.lat)
+    packet2 = sim.get_current()
+    #distance_haver = haversine(packet1.lon, packet1.lat, finish.lon, finish.lat)
+    distance = equirectangular_dist_approx(packet1.lon, packet1.lat, finish.lon, finish.lat)
 
-    print("Distance haver: " + str(distance_haver) + " Distance equire: " + str(distance_equire))
-    if distance_haver > 10:
-        time.sleep(0.05)
+    #print("Distance haver: " + str(distance_haver) + " Distance equire: " + str(distance_equire))
+    C = (packet1.lat, packet1.lon)
+    D = (packet2.lat, packet2.lon)
+    R  = intersects((A,B),(C,D))
+    packet1 = packet2
+    if R:
+        print ("Distance to finish: {:0.1f}m Intersection detected!".format(distance))
     else:
-        time.sleep(2)
+        print ("Distance to finish: {:0.1f}m".format(distance))
+        time.sleep(0.03)
+    if distance < 15:
+        time.sleep(1)
